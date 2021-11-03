f94688859b6d67999f50205c37924674d8d9a7df
Kiran Maniya <vkiranmaniya@gmail.com>
2021-11-04 01:51:43 +0530

    Persist cart items in localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
 import app from '@/store/modules/app'
 import cart from '@/store/modules/cart'
 
@@ -9,5 +10,10 @@ export default new Vuex.Store({
   modules: {
     app,
     cart
-  }
+  },
+  plugins: [
+    createPersistedState({
+      paths: ['cart']
+    })
+  ]
 })
